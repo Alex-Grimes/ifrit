fa758ea92d3f8de3b18509789e5da881aad902c1
alex <alexgrimes.se@gmail.com>
2025-02-26 19:09:16 -0500

    working through template setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Html, response::IntoResponse, routing::get};
 use axum::{http::StatusCode, response::IntoResponse};
 use tokio::net::TcpListener;
 
@@ -13,6 +13,36 @@ pub struct User {
     pub user_name: String,
     pub password: String,
     pub created_date: Date,
+}
+
+async fn user(
+    Path(query_user): Path<String>,
+    State(state): State<Arc<Vec<Post>>>,
+) -> impl IntoResponse {
+    let mut template = UserTemplate {
+        user_name: "none",
+        created_date: "none",
+    };
+
+    for i in 0..state.len() {
+        if query_user == state[i].user_name {
+            template = UserTemplate {
+                user_name: &state[i].user_name,
+                created_date: &state[i].created_date,
+            };
+            break;
+        } else {
+            continue;
+        }
+    }
+    if &template.user_name == &"none" {
+        return (StatusCode::NOT_FOUND, "404 not found").into_response();
+    }
+
+    match template.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "try again later").into_response(),
+    }
 }
 
 async fn index() -> impl IntoResponse {
@@ -36,7 +66,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(index))
-        .route("/users/:user_name", get(user))
+        .route("/user/:user_name", get(user))
         .with_state(shared_state);
 
     let listener = TcpListener::bind("0.0.0.0:4000").await.unwrap();
