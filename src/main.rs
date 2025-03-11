9490ad6c307f183c64f70295ce4d279a7636f0a2
alex <alexgrimes.se@gmail.com>
2025-03-11 19:10:44 -0400

    pivoting to tui for exploration

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,10 @@
-use axum::{Router, response::Html, response::IntoResponse, routing::get};
-use axum::{http::StatusCode, response::IntoResponse};
-use tokio::net::TcpListener;
+const DB_PATH: &str = "./data/db.json";
 
-use sqlx::FromRow;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::types::time::Date;
-
-use std::sync::Arc;
-
-#[derive(FromRow, Debug, Clone)]
-pub struct User {
-    pub user_name: String,
-    pub password: String,
-    pub created_date: Date,
-}
-
-async fn user(
-    Path(query_user): Path<String>,
-    State(state): State<Arc<Vec<Post>>>,
-) -> impl IntoResponse {
-    let mut template = UserTemplate {
-        user_name: "none",
-        created_date: "none",
-    };
-
-    for i in 0..state.len() {
-        if query_user == state[i].user_name {
-            template = UserTemplate {
-                user_name: &state[i].user_name,
-                created_date: &state[i].created_date,
-            };
-            break;
-        } else {
-            continue;
-        }
-    }
-    if &template.user_name == &"none" {
-        return (StatusCode::NOT_FOUND, "404 not found").into_response();
-    }
-
-    match template.render() {
-        Ok(html) => Html(html).into_response(),
-        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "try again later").into_response(),
-    }
-}
-
-async fn index() -> impl IntoResponse {
-    (StatusCode::OK, "Homepage")
-}
-
-#[tokio::main]
-async fn main() {
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://admin:admin@localhost/ifrit-db")
-        .await
-        .expect("couldn't connect to the database");
-
-    let users = sqlx::query_as::<_, User>("select user_name from users")
-        .fetch_all(&pool)
-        .await
-        .unwrap();
-
-    let shared_state = Arc::new(users);
-
-    let app = Router::new()
-        .route("/", get(index))
-        .route("/user/:user_name", get(user))
-        .with_state(shared_state);
-
-    let listener = TcpListener::bind("0.0.0.0:4000").await.unwrap();
-
-    axum::serve(listener, app).await.unwrap();
+#[derive(Serialize, Deserialize, Clone)]
+struct Pet {
+    id: usize,
+    name: String,
+    category: String,
+    age: usize,
+    created_at: DateTime<Utc>,
 }
