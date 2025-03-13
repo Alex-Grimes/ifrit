35d8540ac234a014a0eb244067d12f30f088f07c
alex <alexgrimes.se@gmail.com>
2025-03-12 21:35:46 -0400

    working through command line app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
+use chrono::DateTime;
+use chrono::Utc;
+use crossterm::event;
+use crossterm::terminal::enable_raw_mode;
+use serde::Deserialize;
+use serde::Serialize;
+use thiserror::Error;
+use tokio::io;
+use tokio::sync::mpsc;
+
 const DB_PATH: &str = "./data/db.json";
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -7,4 +20,53 @@ struct Pet {
     category: String,
     age: usize,
     created_at: DateTime<Utc>,
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
+}
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home,
+    Pets,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Pets => 1,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode().expect("can run in raw mode");
+
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+        }
+    })
 }
