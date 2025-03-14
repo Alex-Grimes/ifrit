e3935955e10556780ac9ecf644879af33e7a88eb
alex <alexgrimes.se@gmail.com>
2025-03-13 20:40:31 -0400

    Dont need to use async tokio mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -8,8 +10,19 @@ use crossterm::terminal::enable_raw_mode;
 use serde::Deserialize;
 use serde::Serialize;
 use thiserror::Error;
-use tokio::io;
-use tokio::sync::mpsc;
+use tokio::time::Instant;
+use tui::style::Color;
+use tui::style::Style;
+use tui::widgets::Block;
+use tui::widgets::BorderType;
+use tui::widgets::Borders;
+use tui::widgets::Paragraph;
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+use tui::layout::Constraint;
+use tui::layout::Direction;
+use tui::layout::Layout;
+use tui::widgets::ListState;
 
 const DB_PATH: &str = "./data/db.json";
 
@@ -68,5 +81,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         }
-    })
+    });
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
+    let mut active_menu_item = MenuItem::Home;
+    let mut pet_list_state = ListState::default();
+    pet_list_state.select(Some(0));
+
+    loop {
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Length(3),
+                    ]
+                    .as_ref(),
+                )
+                .split(size);
+
+            let copyright = Paragraph::new("pet-CLI 2025 - all rights reserved")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Center)
+                .block(Block::default().borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .title("Copyright")
+                .border_type(BorderType::Plain);
+        );
+    }
 }
